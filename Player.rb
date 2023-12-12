ddc8c0ac2f4d5dd7f7a738d9b0f59dffb7797243
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-12 06:44:02 -0800

    Describing the player.rb file

--- Player.rb
+++ Player.rb
@@ -6,8 +6,10 @@ class Player
 ## adding the reader to be able to perfom the name when it's being called 
   attr_reader :name
 
+## We will need the initialize to start with the game, 
   def initialize(name)
     @name = name
+## as part of the parameters we have to start with 3 lives each player
     @lives = 3
   end
 
--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+## calling the folders the program needs to run
 require './Game'
 require './Player'
 
+
+## the game function has to be call for the game to start in the main folder
 Game.new()
