ff78ed4ca3e882997159875815b8972a45688c2d
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-12 06:00:14 -0800

    Completing task 1, writing the players rules

---
+++ Player.rb
@@ -0,0 +1,13 @@
+class Player
+
+  # adding the accessor of lives to be able to define getter and setters in a better way
+  attr_accessor :lives
+
+  
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+end
